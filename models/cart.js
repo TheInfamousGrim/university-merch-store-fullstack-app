15bff5be0c0493ed3459b23a05b8f9f01395394d
TheInfamousGrim <finchergeorge1@gmail.com>
2022-10-17 13:12:57 +0100

    Create cart db model
    Created a model for the users cart in cart.js
    
    fixed typos in user.js.

---
+++ models/cart.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+
+// Import db
+const sequelize = require('../config/connection');
+
+// Initialize cart model (table) by extending off Sequilize's Model Class
+class Cart extends Model {}
+
+// Set up fields and rules for Cart model
+Cart.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id',
+            },
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        purchase_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'cart',
+    }
+);
