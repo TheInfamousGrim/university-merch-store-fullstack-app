10e7ed9fb1433e8060dce318ffa179160163efa7
TheInfamousGrim <finchergeorge1@gmail.com>
2022-10-18 05:36:43 +0100

    Create search product event listener
    Created a function to search for a product.
    
    Cleaned up the ui of the main.handlebars
    template.

---
+++ public/js/search.js
@@ -0,0 +1,14 @@
+// get search bar element
+
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+
+const searchEvent = async (event) => {
+    const search = searchInput.value;
+    console.log(search);
+    // get user search input converted to lowercase
+    document.location.replace(`/product/${search}`);
+};
+
+// listen for user events
+searchBtn.addEventListener('submit', searchEvent);
