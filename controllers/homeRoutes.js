e931a495581edd66272930be7c3780f0f8a5defb
TheInfamousGrim <finchergeorge1@gmail.com>
2022-10-18 03:02:27 +0100

    Create search item route in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Op } = require('sequelize');
 const { User, Product } = require('../models');
 
 router.get('/', async (req, res) => {
@@ -9,6 +10,32 @@ router.get('/', async (req, res) => {
         // Serialize data so the template engine can read it
         const products = productData.map((productItem) => productItem.get({ plain: true }));
 
+        res.render('homepage', {
+            products,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/:name', async (req, res) => {
+    try {
+        console.log(req.params.name);
+        const productData = await Product.findAll({
+            where: {
+                name: {
+                    [Op.substring]: req.params.name,
+                },
+            },
+        });
+
+        if (!productData) {
+            res.status(404).json({ message: 'No products with this id!' });
+            return;
+        }
+
+        const products = productData.map((productItem) => productItem.get({ plain: true }));
+        console.log(products);
         res.render('homepage', {
             products,
         });
