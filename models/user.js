fafc9ee51bff8ca9d26c9591a8a1173b125d38bb
TheInfamousGrim <finchergeorge1@gmail.com>
2022-10-18 16:15:15 +0100

    Fix db seeding issue

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
+            unique: 'email',
             validate: {
                 isEmail: true,
             },
